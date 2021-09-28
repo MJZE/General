631df707b65958912e1f5ea15929c3caac5365e7
MJZE <87388670+MJZE@users.noreply.github.com>
2021-09-28 22:32:40 +0200

    Add files via upload

---
+++ Opdracht011/Index.js
@@ -0,0 +1,22 @@
+const age = 12
+
+if (age >= 18){
+    console.log("Je mag naar binnen!");
+} else {
+    console.log("Helaas, we mogen jou niet binnen laten");
+}
+
+const isFemale = false
+
+if(isFemale){
+    console.log("Wist je al van onze ladies night?");
+} else {
+    console.log("Jij bent helaas niet welkom op onze ladies night");
+}
+
+const driverStatus = "bob"
+if(driverStatus="bob"){
+    console.log("Jij mag naar huis rijden")
+} else{
+    console.log("Vanavond wordt het een taxi voor jou")
+}
