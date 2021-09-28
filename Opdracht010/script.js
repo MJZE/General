b93e885d3ec799e22145d032d0fdd94eee50ace3
MJZE <87388670+MJZE@users.noreply.github.com>
2021-09-28 22:01:40 +0200

    Add files via upload

---
+++ Opdracht010/script.js
@@ -0,0 +1,7 @@
+let firstVariable = 1230941
+let secondVariable = 1823794
+//Dit is een grote som
+/*console.log(firstVariable*secondVariable)
+let thirdVariable = 637263
+let fourthVariable = 54
+console.log(thirdVariable/fourthVariable)*/
