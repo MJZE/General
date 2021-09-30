2d6b0d952b8f863c2f6f66e6e7de9e47aac66115
MJZE <87388670+MJZE@users.noreply.github.com>
2021-09-30 21:21:02 +0200

    Add files via upload

---
+++ Opdracht014/Index.js
@@ -0,0 +1,21 @@
+const makePizza = function() {
+    console.log("Prepare supplies");
+    console.log("Make the dough");
+    console.log("Add tomato sauce")
+    console.log("Add the pizza toppings")
+    console.log("Prepare the pizza in the oven")
+}
+
+
+
+const makeSushi = function() {
+    console.log("Make the sushi rice")
+    console.log("Cut the ingredients")
+    console.log("Lay down the Nori")
+    console.log("Roll the Nori, rice and ingredients")
+    console.log("Cut the sushi roll")
+    console.log("Put soja sauce, wasabi and ginger on the side")
+
+}
+makeSushi(), makeSushi(), makeSushi()
+makePizza, makePizza()
