3119e7cee5be6df0cdb8162405bf67536deaed72
MJZE <87388670+MJZE@users.noreply.github.com>
2021-09-30 21:55:01 +0200

    Add files via upload

---
+++ Opdracht015/Index.js
@@ -0,0 +1,7 @@
+const paintWall = function(color, location) {
+    console.log("The "+ location +  " wall has been painted " + color)
+}
+
+paintWall("green", "south")
+paintWall("north", "blue")
+paintWall()
