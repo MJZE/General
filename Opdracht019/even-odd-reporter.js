30d7d74796250e89f0733a2d5d351402c7f56cc7
MJZE <87388670+MJZE@users.noreply.github.com>
2021-10-03 16:46:13 +0200

    Add files via upload

---
+++ Opdracht019/even-odd-reporter.js
@@ -0,0 +1,9 @@
+for (let i =0; i<=20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    var evenOdd = (i % 2)
+    if (evenOdd == 0) {console.log(`${i} is even`)} else {console.log(`${i} is odd`)}
+}
+
+
+
+
---
+++ Opdracht019/multiplication-tables.js
@@ -0,0 +1,7 @@
+for (let i=1; i<=10; i++) {
+    console.log(`This is the table of ${i}`)
+    for (let a=1;a<=10; a++) {
+        var multiply = a*i
+        console.log(`${i} x ${a} = ${multiply}`)
+    }
+}
